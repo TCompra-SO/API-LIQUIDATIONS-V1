6c4750016cb8e6e25eab3d064133f98d1d7cd7ae
EdwinLlave <maxedwin8@gmail.com>
2025-02-21 12:10:47 -0500

    Se han hecho las correciones

--- src/services/offerService.ts
+++ src/services/offerService.ts
@@ -1257,6 +1257,9 @@ export class OfferService {
             $set: {
               stateID: RequirementState.PUBLISHED,
             },
+            $inc: {
+              number_offers: -1, // Resta 1 a numOffers
+            },
           }
         );
         return {
