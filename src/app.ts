0666a854bd52807aff17fdaa736a95046f2ba244
al.h8500@gmail.com <al.h8500@gmail.com>
2025-07-10 16:14:49 -0500

    Obtener token de cookies

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import "dotenv/config";
 import cors from "cors";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 import { RootRouter } from "./routes/RootRouter";
 import "./initConfig";
 export class App {
@@ -10,9 +11,15 @@ export class App {
   static getInstance() {
     if (!App.instance) {
       App.instance = express();
+      App.instance.use(cookieParser());
       App.instance.use(bodyParser.urlencoded({ extended: false }));
       App.instance.use(bodyParser.json());
-      App.instance.use(cors());
+      App.instance.use(
+        cors({
+          origin: process.env.URL_FRONTEND,
+          credentials: true,
+        })
+      );
       App.instance.use(RootRouter.getRouter());
     }
     return App.instance;
