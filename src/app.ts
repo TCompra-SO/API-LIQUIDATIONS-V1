0c02577475894b0636af836af32ac0af5d985719
Alexandra1123 <al.h8500@gmail.com>
2025-07-15 00:13:38 +0400

    añadir otros servidores a cors

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,14 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import { RootRouter } from "./routes/RootRouter";
 import "./initConfig";
+
+const allowedOrigins = [
+  process.env.URL_FRONTEND,
+  process.env.API_PRODUCTS,
+  process.env.API_SERVICES,
+  process.env.API_USER,
+];
+
 export class App {
   private static instance: Express;
 
@@ -16,7 +24,13 @@ export class App {
       App.instance.use(bodyParser.json());
       App.instance.use(
         cors({
-          origin: process.env.URL_FRONTEND,
+          origin: (origin, callback) => {
+            if (allowedOrigins.includes(origin)) {
+              callback(null, true);
+            } else {
+              callback(new Error("Not allowed by CORS"));
+            }
+          },
           credentials: true,
         })
       );
